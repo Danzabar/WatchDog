4a925ea7f140473df664201a3f18d6f5d057af37
Dan Cox <danzabian@gmail.com>
2017-04-13 08:29:48 +0100

    Adding pongo

--- site/config.go
+++ site/config.go
@@ -2,9 +2,17 @@ package site
 
 import (
     "github.com/Danzabar/WatchDog/core"
+    "github.com/flosch/pongo2"
+)
+
+var (
+    Template *pongo2.TemplateSet
 )
 
 func Setup() {
+    fs := pongo2.MustNewLocalFileSystemLoader("templates/")
+    Template = pongo2.NewSet("Templates", fs)
+
     // Ping route for AWS Cloudwatch
     core.App.Router.HandleFunc("/ping", Ping)
 
