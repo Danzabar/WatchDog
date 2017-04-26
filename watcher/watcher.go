360502e881e452fec8163a0e9a5f1a0af9aecca7
Dan Cox <danzabian@gmail.com>
2017-04-26 08:06:06 +0100

    Adding missing response time parameter for degredations

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -58,7 +58,7 @@ func AnalyseStatus(a *core.Audit, s core.Subject) string {
     if a.ResponseTime > s.ResponseLimit {
         // Degredation? Yes please
         Shout.SendAlert(
-            fmt.Sprintf("%s domain has entered degredated status - response time was %d", s.Domain),
+            fmt.Sprintf("%s domain has entered degredated status - response time was %d", s.Domain, a.ResponseTime),
             fmt.Sprintf("DEGREDATION: %s", s.Name),
         )
         return DEGREDATED
