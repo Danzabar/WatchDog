0a6fe5395312e597574138dbe1ec16dfbcfdc65b
Dan Cox <danzabian@gmail.com>
2017-08-23 07:31:43 +0100

    Using error string to satisfy glide

--- core/api.go
+++ core/api.go
@@ -1,36 +1,36 @@
 package core
 
 import (
-    "encoding/json"
+	"encoding/json"
 )
 
 type RestResponse struct {
-    ErrorCode string            `json:"errorCode,omitempty"`
-    Error     string            `json:"error,omitempty"`
-    Errors    map[string]string `json:"errors,omitempty"`
-    Message   string            `json:"message,omitempty"`
-    Status    bool              `json:"status,omitempty"`
-    Data      []interface{}     `json:"data,omitempty"`
+	ErrorCode string            `json:"errorCode,omitempty"`
+	Error     string            `json:"error,omitempty"`
+	Errors    map[string]string `json:"errors,omitempty"`
+	Message   string            `json:"message,omitempty"`
+	Status    bool              `json:"status,omitempty"`
+	Data      []interface{}     `json:"data,omitempty"`
 }
 
 func (r *RestResponse) Serialize() []byte {
-    str, err := json.Marshal(r)
+	str, err := json.Marshal(r)
 
-    if err != nil {
-        App.Log.Error(err)
-    }
+	if err != nil {
+		App.Log.Error(err.Error())
+	}
 
-    return str
+	return str
 }
 
 type SubjectResponse struct {
-    Subject Subject `json:"subject"`
-    Audits  []Audit `json:"audits,omitempty"`
+	Subject Subject `json:"subject"`
+	Audits  []Audit `json:"audits,omitempty"`
 }
 
 func NewSubjectResponse(s Subject, a []Audit) ([]byte, error) {
-    s.User = ""
-    s.Pass = ""
+	s.User = ""
+	s.Pass = ""
 
-    return json.Marshal(&SubjectResponse{s, a})
+	return json.Marshal(&SubjectResponse{s, a})
 }
--- core/application.go
+++ core/application.go
@@ -1,19 +1,19 @@
 package core
 
 import (
-    "github.com/go-playground/locales/en"
-    ut "github.com/go-playground/universal-translator"
-    "github.com/gorilla/mux"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/mssql"
-    _ "github.com/jinzhu/gorm/dialects/mysql"
-    _ "github.com/jinzhu/gorm/dialects/postgres"
-    _ "github.com/jinzhu/gorm/dialects/sqlite"
-    "github.com/op/go-logging"
-    "gopkg.in/go-playground/validator.v9"
-    translations "gopkg.in/go-playground/validator.v9/translations/en"
-    "net/http"
-    "os"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/op/go-logging"
+	"gopkg.in/go-playground/validator.v9"
+	translations "gopkg.in/go-playground/validator.v9/translations/en"
+	"net/http"
+	"os"
 )
 
 var App *Application
@@ -22,49 +22,49 @@ var App *Application
  * The Application Struct
  */
 type Application struct {
-    DB         *gorm.DB
-    Router     *mux.Router
-    Log        *logging.Logger
-    Alerts     bool
-    Port       string
-    Validator  *validator.Validate
-    Translator ut.Translator
+	DB         *gorm.DB
+	Router     *mux.Router
+	Log        *logging.Logger
+	Alerts     bool
+	Port       string
+	Validator  *validator.Validate
+	Translator ut.Translator
 }
 
 func NewApp(port string, dbDriver string, dbCreds string, alerts bool) {
-    db, _ := gorm.Open(dbDriver, dbCreds)
-    SetLogging()
+	db, _ := gorm.Open(dbDriver, dbCreds)
+	SetLogging()
 
-    App = &Application{
-        DB:     db,
-        Router: mux.NewRouter(),
-        Log:    logging.MustGetLogger("scribe"),
-        Port:   port,
-        Alerts: alerts,
-    }
+	App = &Application{
+		DB:     db,
+		Router: mux.NewRouter(),
+		Log:    logging.MustGetLogger("scribe"),
+		Port:   port,
+		Alerts: alerts,
+	}
 
-    en := en.New()
-    uni := ut.New(en, en)
+	en := en.New()
+	uni := ut.New(en, en)
 
-    App.Translator, _ = uni.GetTranslator("en")
-    App.Validator = validator.New()
+	App.Translator, _ = uni.GetTranslator("en")
+	App.Validator = validator.New()
 
-    translations.RegisterDefaultTranslations(App.Validator, App.Translator)
+	translations.RegisterDefaultTranslations(App.Validator, App.Translator)
 }
 
 /**
  * Sets up logging for the application
  */
 func SetLogging() {
-    f := logging.MustStringFormatter(`%{color}%{time:15:04:05.000} %{shortfunc} -> %{level:.4s}%{color:reset} %{message}`)
-    b := logging.NewLogBackend(os.Stderr, "", 0)
+	f := logging.MustStringFormatter(`%{color}%{time:15:04:05.000} %{shortfunc} -> %{level:.4s}%{color:reset} %{message}`)
+	b := logging.NewLogBackend(os.Stderr, "", 0)
 
-    bf := logging.NewBackendFormatter(b, f)
-    logging.SetBackend(bf)
+	bf := logging.NewBackendFormatter(b, f)
+	logging.SetBackend(bf)
 }
 
 func (a *Application) Run() {
-    http.Handle("/", a.Router)
-    a.Log.Debugf("Running app on " + a.Port)
-    a.Log.Critical(http.ListenAndServe(a.Port, nil))
+	http.Handle("/", a.Router)
+	a.Log.Debugf("Running app on " + a.Port)
+	a.Log.Critical(http.ListenAndServe(a.Port, nil).Error())
 }
